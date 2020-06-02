6bb4e3a85e4d36230b23b5092a94e3a27cf3a818
Sickology101 <66300097+Sickology101@users.noreply.github.com>
2020-06-02 11:22:30 +0300

    Add files via upload
    A web page that uses SQL databases to store information such as email addresses encrypted passwords and realtime updated and saved text that the user inputs.

---
+++ SecretDiary/connection.php
@@ -0,0 +1,6 @@
+<?php
+$link = mysqli_connect("shareddb-r.hosting.stackcp.net","secretdi-313233986a","e01vhoms1i","secretdi-313233986a");
+       if (mysqli_connect_error()){
+           die ("Database Connection Error");
+       }
+?>
---
+++ SecretDiary/header.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <style type="text/css">
+        .container {
+            text-align: center;
+            width: 400px;
+        }
+        #homePageContainer {
+            margin-top: 150px;
+        }
+        html { 
+          background: url(background.jpg) no-repeat center center fixed; 
+          -webkit-background-size: cover;
+          -moz-background-size: cover;
+          -o-background-size: cover;
+          background-size: cover;
+        }
+        body{
+            background: none;
+        }
+        #logIn {
+            display: none;
+        }
+        .toggleForms {
+            font-weight: bold;
+        }
+        a:hover {
+             cursor:pointer;
+        }
+        #diary {
+            width:100%;
+            height:100%;
+        }
+        #containerLoggedIn {
+            margin-top:30px;
+        }
+    </style>
+  </head>
+  <body>
